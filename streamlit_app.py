78a53834479d35663c4ad286d2877e5c93f5a1ac
Atulya Srivastava <90210294+Atulya199@users.noreply.github.com>
2023-05-29 14:05:20 +0530

    Update streamlit_app.py
    corrected line of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,13 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-      except URLError as e:
-        streamlit.error()
-
-
-streamlit.write('The user entered ', fruit_choice)
+ except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
